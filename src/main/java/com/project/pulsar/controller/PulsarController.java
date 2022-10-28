c2afdbcf78d6451713e35a56b201087d8923cd67
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-28 19:17:48 -0300

    Inserindo valores de padrao de producao e resposta

--- src/main/java/com/project/pulsar/controller/PulsarController.java
+++ src/main/java/com/project/pulsar/controller/PulsarController.java
@@ -1,5 +1,6 @@
 package com.project.pulsar.controller;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,11 +9,12 @@ import javax.ws.rs.core.Response;
 
 import com.project.pulsar.dto.PulsarDto;
 
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 @Path("/v1/project/pulsar/")
 public class PulsarController {
 
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response save(PulsarDto pulsarDto) {
 		return Response.ok().build();
 	}
