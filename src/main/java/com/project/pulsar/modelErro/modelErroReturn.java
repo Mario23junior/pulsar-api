ecea18aa72c5ee3390ad9fe9bf4c1fcb158870c0
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-29 15:55:36 -0300

    Criando modelo de dados de captura

---
+++ src/main/java/com/project/pulsar/modelErro/modelErroReturn.java
@@ -0,0 +1,34 @@
+package com.project.pulsar.modelErro;
+
+public class modelErroReturn {
+
+	private String field;
+	private String message;
+
+	public modelErroReturn(String field, String message) {
+		super();
+		this.field = field;
+		this.message = message;
+	}
+
+	public modelErroReturn() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
