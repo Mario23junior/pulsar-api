c5b062512a9c9b0c89f2e75b45806def03f1e52d
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-29 01:06:30 -0300

    Inserindo dados no banco de dados

--- src/main/java/com/project/pulsar/controller/PulsarController.java
+++ src/main/java/com/project/pulsar/controller/PulsarController.java
@@ -1,5 +1,6 @@
 package com.project.pulsar.controller;
 
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,6 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.project.pulsar.dto.PulsarDto;
+import com.project.pulsar.model.Pulsar;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,9 +18,18 @@ import com.project.pulsar.dto.PulsarDto;
 public class PulsarController {
 
 	@POST
+	@Transactional
 	public Response save(PulsarDto pulsarDto) {
-		return Response.ok(pulsarDto).build();
-	}
+		Pulsar p = new Pulsar();
+		p.setNome(pulsarDto.getNome());
+		p.setImgSimulacao(pulsarDto.getImgSimulacao());
+		p.setNomeConstelacao(pulsarDto.getNomeConstelacao());
+		p.setDistancia(pulsarDto.getDistancia());
+		p.setAscReta(pulsarDto.getAscReta());
+		p.persist();
+		return Response.ok(p).build();
+	
+ 	}
 	
 	@GET
 	public Response listAll() {
---
+++ src/main/java/com/project/pulsar/model/Pulsar.java
@@ -0,0 +1,79 @@
+package com.project.pulsar.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+@Table(name = "PULSAR")
+public class Pulsar extends PanacheEntityBase {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	@Column(length = 200)
+	private String imgSimulacao;
+	private String nomeConstelacao;
+	private String ascReta;
+	private Double distancia;
+
+	public Pulsar() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getImgSimulacao() {
+		return imgSimulacao;
+	}
+
+	public void setImgSimulacao(String imgSimulacao) {
+		this.imgSimulacao = imgSimulacao;
+	}
+
+	public String getNomeConstelacao() {
+		return nomeConstelacao;
+	}
+
+	public void setNomeConstelacao(String nomeConstelacao) {
+		this.nomeConstelacao = nomeConstelacao;
+	}
+
+	public String getAscReta() {
+		return ascReta;
+	}
+
+	public void setAscReta(String ascReta) {
+		this.ascReta = ascReta;
+	}
+
+	public Double getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(Double distancia) {
+		this.distancia = distancia;
+	}
+
+}
