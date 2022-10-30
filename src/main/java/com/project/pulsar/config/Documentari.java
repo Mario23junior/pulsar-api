27e3ce19ffda33e7b9c3052952de2fda0813d7f8
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-30 02:09:50 -0300

    removendo flags de acesso

--- src/main/java/com/project/pulsar/config/Documentari.java
+++ src/main/java/com/project/pulsar/config/Documentari.java
@@ -8,21 +8,21 @@ import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-@OpenAPIDefinition(//
-	    tags = { //
-	        @Tag(name = "API de listagem de pulsares registrados"), //
+@OpenAPIDefinition(
+	    tags = { 
+	        @Tag(name = "API de listagem de pulsares registrados"), 
 	        @Tag(name = "Pulsar-API", description = "O bjetivo e ter informaçãoes sobre o fato, do desligamento parece ocorrer aproximadamente após 10 a 100 milhões de anos.")//
-	    }, //
-	    info = @Info(//
-	        title = "API Pulsar", //
-	        version = "1.0.0", //
-	        contact = @Contact(//
+	    }, 
+	    info = @Info(
+	        title = "API Pulsar", 
+	        version = "1.0.0", 
+	        contact = @Contact(
 	            name = "Pulsar é uma estrela de nêutrons que, em virtude de seu intenso campo magnético, transforma a energia rotacional em energia eletromagnética."
 	            		+ " A medida que o pulsar gira, seu intenso campo magnético induz um enorme campo elétrico na sua superfície.", //
-	            url = "https://github.com/Mario23junior")), //
-	    servers = { //
-	        @Server(url = "http://localhost:8080")//
-	    }) //
+	            url = "https://github.com/Mario23junior")), 
+	    servers = { 
+	        @Server(url = "http://localhost:8080")
+	    }) 
 public class Documentari extends Application {
 
 }
